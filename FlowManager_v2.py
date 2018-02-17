2744c1d071f7200c082ea86c5aaca80e8bf3cbb5
bernablues <bernadettemisa403@gmail.com>
2018-02-17 15:36:56 +0800

    Flow Manager per field checker

--- FlowManager_v2.py
+++ FlowManager_v2.py
@@ -1,6 +1,3 @@
-# Use RegExp para sa mga wildcards!
-
-
 #bundleData = [type, bundleSeq, sid, ipAddr, dataSummary (eto yung minMaxAve, etc)]
 #flowTable = [ruleNo, type, bundleSeq, sid, ipAddr, dataSummary (eto yung minMaxAve, etc), action]
   
@@ -42,168 +39,64 @@
 
 class FlowManager:
 
-    def __init__(self, bundleData, flowTable):
-        self.bundleData = bundleData
+    def __init__(self, flowTable):
         self.flowTable = flowTable
 
-        self.type = bundleData[0]
-        self.seq = bundleData[1]
-        self.sid = bundleData[2]
-        self.ipAddr = bundleData[3]
-        self.dataSummary = bundleData[4]
 
-    # def getBundleData(bundleData):
-    #     self.bundleData = bundleData
-    #     self.type = bundleData[0]
-    #     self.seq = bundleData[1]
-    #     self.sid = bundleData[2]
-    #     self.ipAddr = bundleData[3]
-    #     self.dataSummary = bundleData[4]
-
-    def getType(self):
-        return self.type
-
-    def getSeq(self):
-        return self.seq
-
-    def getSID(self):
-        return self.sid
-
-    def getIPaddr(self):
-        return self.ipAddr
-
-    def getDataSummary(self):
-        return self.dataSummary
-
-    def parseDataSummary(self):
-        pass
-
-
-    def getFlowTableLength(self):
-        # Define flow table here?
-        # flowTable = [[ruleNo(int), ipAddr(str ), field2, field3, action(int)]]
-        # flowTable = flowTable=[[1, 'b', 3, 'x', 'action'], [2, 'c', 3, 'y', 'action']]
-        self.flowTableLength = len(self.flowTable)
-        return self.flowTableLength
+    # def getFlowTableLength(self):
+    #     self.flowTableLength = len(self.flowTable)
+    #     return len(self.flowTableLength)
 
     def getFlowTable(self):
-        # Define flow table here?
-        # flowTable = [[ruleNo(int), ipAddr(str ), field2, field3, action(int)]]
-        # flowTable = flowTable=[[1, 'b', 3, 'x', 'action'], [2, 'c', 3, 'y', 'action']]
         # print self.flowTable
         return self.flowTable
 
-    # def getMiniFlowTable(self):
+    def getBundleData(self, bundleData):
+        self.bundleData = bundleData
+        return self.bundleData
 
-    #     self.Mini = self.getFlowTable()
-    #     lenz = len(self.Mini)+1
-    #     for index_x, x in enumerate(self.Mini):
-    #         for index_y, y in enumerate(x):
-    #             self.Mini[index_x][index_y] = self.Mini[index_x][1:lenz]
-            
-    #     return self.Mini
+    def matchFlow(self, bundleData):
+        self.bundleData = bundleData
+
+        bundleLen = len(bundleData)
+        rule_length = len(self.flowTable[0][1:-1])
     
+        matchedFlowAction = 'No Flow Action Found'
         
-    # def flowMatching(self):
-    #     intersection_a = [list(filter(lambda x: x in self.bundleData, sublist)) for sublist in self.flowTable]
+        for sensorData in range(0, bundleLen):
+            # print "sensorData", sensorData
+            # Iterates per row
+            for ruleIndex, rule in enumerate(self.flowTable):
+                print "==="
+                print "rule #:" + str(ruleIndex) + "rule: " + str(rule)
+                print "==="
+                print ""
 
-    #     # Returns list ng intersections ng bundleData and flowTable
-    #     # print "intersection", (intersection_a)
+                # Iterates per field(columb) in flow rule(row)
+                ctr = 0
+                for fieldIndex, ruleField in enumerate(rule[1:-1]):
+                    # print "ruleField: bundleData: ", ruleField, self.bundleData[sensorData][fieldIndex]
+                    
+                    if(self.bundleData[sensorData][fieldIndex] == ruleField or ruleField=='*'):
+                        # print "fieldIndex: " + str(fieldIndex) + " ruleField: " + str(ruleField) + " bundleData: " + self.bundleData[sensorData][fieldIndex]                        
+                        ctr = ctr+1
+                 
+                # Checks if kumagat sa certain flow rule
+                if(ctr == rule_length):
 
-     
-    #     try:
+                    # print ""
+                    # print "==="
+                    matchedFlowIndex = ruleIndex
+                    # print "matchedFlowRule (Index): ", matchedFlowIndex 
+                    
+                    matchedFlowRule = self.flowTable[ruleIndex]
+                    # print "Flow Rule", matchedFlowRule
 
-    #         # Gets the index of the matching flow
-    #         matchedFlowIndex = intersection_a.index(self.bundleData)
-    #         print "index ng intersection", matchedFlowIndex
-
-    #         # Gets the matching flow (list/whole row)
-    #         matchedFlow = self.flowTable[matchedFlowIndex]
-    #         print "matched flow index", matchedFlow
-
-    #         # Gets the matching flow ruleNo
-    #         matchedFlowRuleNo = self.flowTable[matchedFlowIndex][0]
-    #         print "matched flow rule", matchedFlowRuleNo
-
-    #         # Gets corresponding action of matched flow 
-    #         matchedFlowAction = self.flowTable[matchedFlowIndex][6]
-    #         print "matched flow action", matchedFlowAction
-
-    #         return matchedFlowAction
-
-    #     except ValueError:
-    #         print "No Matching Flow"           
-
-    def flowMatching_v2(self):
-        # Mano manong iteration to be able to check each field
-        # For RegExp purposes
-        # wildcards*
-        rule_length = len(flowTable[0])
-
-        # Iterates per row
-        for index_i, i in enumerate(flowTable):
-            # print bundle_date[index_i], index_i, i
-
-            # Iterates per elem in row rule
-            for index_j, j in enumerate(i):
-                # If same dun sa element, or wildcard
-                if((bundle_date[index_i][index_j] == j) || (j=='*')):
-                    # print index_j, j
-                    ctr = ctr+1
-                    # per match, +1 sa counter
-                else:
+                    matchedFlowAction = self.flowTable[matchedFlowIndex][-1]
                     break
-
-            if(ctr == rule_length):
-                matchedFlowIndex = index_i
-                matchedFlowAction = flowTable_master[matchedFlowIndex][-1]
-            #     # if ctr == sa number of columns ng flow table, ibig sabihin match.
-            #     # save index number to a var
-            #     # exit then return yung index number nun.
-                break
-
-        # return action_integer dapat to eh
-        # returns string
-        return matchedFlowAction
-
-
-
         
-        # v1 demo================================
-        # intersection_a = [list(filter(lambda x: x in self.bundleData, sublist)) for sublist in self.flowTable]
-
-        # # Returns list ng intersections ng bundleData and flowTable
-        # # print "intersection", (intersection_a)
-
-     
-        # try:
-
-        #     # Gets the index of the matching flow
-        #     matchedFlowIndex = intersection_a.index(self.bundleData)
-        #     print "index ng intersection", matchedFlowIndex
-
-        #     # Gets the matching flow (list/whole row)
-        #     matchedFlow = self.flowTable[matchedFlowIndex]
-        #     print "matched flow index", matchedFlow
-
-        #     # Gets the matching flow ruleNo
-        #     matchedFlowRuleNo = self.flowTable[matchedFlowIndex][0]
-        #     print "matched flow rule", matchedFlowRuleNo
-
-        #     # Gets corresponding action of matched flow 
-        #     matchedFlowAction = self.flowTable[matchedFlowIndex][6]
-        #     print "matched flow action", matchedFlowAction
-
-        #     return matchedFlowAction
-
-        # except ValueError:
-        #     print "No Matching Flow"   
-
-    def __getIntersection(self):
-        intersection_a = [list(filter(lambda x: x in self.bundleData, sublist)) for sublist in self.flowTable]
-        # Returns list ng intersections ng bundleData and flowTable
-        print "intersection", (intersection_a)
-        return intersection_a
+        # print "matchedFlowAction:", matchedFlowAction
+        return matchedFlowAction
 
     def __getMatchedFlowIndex(self):
         matchedFlowIndex = __getIntersection().index(self.bundleData)
@@ -248,34 +141,26 @@ class FlowManager:
     
 def main():
 
-    bundleData=['1', '4', '1', '172.24.1.1', '']
-    match_flowTable=[['1', '1', '4', '1', '172.24.1.2', '', '1'], ['2', '1', '4', '1', '172.24.1.1', '', '0'], ['3', '1', '4', '1', '172.24.1.3', '', '5']]
-    noMatch_flowTable=[['1', '1', '4', '1', '172.24.1.2', '', '1'], ['3', '1', '4', '1', '172.24.1.3', '', '5']]
-
-
-    flow = FlowManager(bundleData, match_flowTable)
-    flow.getFlowTable()
-
-
-    x = flow.flowMatching()
-    print "return value", x
-
-    flow.createNewFlow(['3', '5', '2', '172.24.1.5', ''])
-
-    print""
-
-    print "flowTable"
+    # ==================================== v2 demo
+    # bundleData=[['1', '4', '1', '172.24.1.1', '']]
+    bundleData=[['5', '4', '5', '5', ''], ['6', '4', '6', '5', ''], ['1', '4', '1', '172.24.1.1', '']]
+    flowTable_mini=[['1', '4', '1', '172.24.1.2', ''], ['1', '4', '1', '172.24.1.1', ''], ['1', '4', '1', '172.24.1.3', '']]
+    flowTable_master=[['1', '1', '4', '1', '172.24.1.2', '', '1'], ['2', '1', '4', '1', '172.24.1.1', '', '0'], ['3', '1', '4', '1', '172.24.1.3', '', '5']]
+    
+    flow = FlowManager(flowTable_master)
+    
+    print "Getting Master Flow Table"
     print(flow.getFlowTable())
 
-    # flow__getIntersection()
-    # flow.__getMatchedFlowIndex()
+    print""
+    print "Get bundleData"
+    print(flow.getBundleData(bundleData))
 
 
-    # ==================================== v2 demo
-    bundleData=['1', '4', '1', '172.24.1.1', '']
-    flowTable_master=[['1', '1', '4', '1', '172.24.1.2', ''], ['2', '1', '4', '1', '172.24.1.1', ''], ['3', '1', '4', '1', '172.24.1.3', '']]
-    flowTable_mini=[['1', '1', '4', '1', '172.24.1.2', '', '1'], ['2', '1', '4', '1', '172.24.1.1', '', '0'], ['3', '1', '4', '1', '172.24.1.3', '', '5']]
-    
-    
+    print""
+    print "Matching flows"
+    x = flow.matchFlow(bundleData)
+    print "Matched Flow Action", x
+
 if __name__ == "__main__":
     main()
